fc1ec8f6f43028acd39abf77a2d5bf959bd3a831
gendyvip <mohamed20elgendy@gmail.com>
2022-08-21 18:03:18 +0200

    Combining Reducers

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,7 +1,19 @@
-import { createStore } from "redux";
-import { Reducer, initialState } from './reducer';
+import { createStore,combineReducers } from "redux";
+import { Dishes } from './dishes';
+import { Promotions} from './promotions';
+import { Comments } from './comments';
+import { Leaders } from './leaders';
+
+
 
 export const ConfigureStore = () => {
-    const store = createStore(Reducer,initialState);
+    const store = createStore(
+        combineReducers({
+            dishes:Dishes,
+            comments:Comments,
+            leaders:Leaders,
+            promotions:Promotions
+        })
+    );
     return store;
 };
